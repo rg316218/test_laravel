0e9e76bde5403575cc3838f366a414aedd1dd7a0
rg316218 <132314932+rg316218@users.noreply.github.com>
2023-09-05 15:23:56 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        //
+        if(\App::environment(['production'])){
+            \URL::forceScheme('https');
+        }
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
 }
